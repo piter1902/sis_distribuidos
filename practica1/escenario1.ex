b1dddd1f3f9b6f1ed34e6460ab4b8148d498d2d9
Pedro Tamargo <pedro.tamargo.allue@gmail.com>
2019-10-01 18:26:28 +0200

    Intento de acabar el escenario 1

--- practica1/escenario1.ex
+++ practica1/escenario1.ex
@@ -1,11 +1,27 @@
-import Fib
-defmodule Servidor do
-	def server() do
-		[op | tail] = receive do	
-            l -> l
-        end
-        
-        empty_list = cond do
-            op == :fib -> Enum.map(tail, fn x -> Fib.fibonacci x end) 
-        end
+# AUTORES: nombres y apellidos
+# NIAs: números de identificacion de los alumnos
+# FICHERO: nombre del fichero
+# FECHA: fecha de realizacion
+# TIEMPO: tiempo en horas de codificación
+# DESCRIPCION: breve descripcion del contenido del fichero
 
+import Fib
+
+defmodule Servidor do
+  def server() do
+    [op | tail] =
+      receive do
+        l -> l
+      end
+
+    fib_list =
+      cond do
+        op == :fib -> Enum.map(tail, fn x -> Fib.fibonacci(x) end)
+        op == :fib_tr -> Enum.map(tail, fn x -> Fib.fibonacci_tr(x) end)
+        op == :of -> Enum.map(tail, fn x -> Fib.of(x) end)
+      end
+
+    send(pid, fib_list)
+    server()
+  end
+end
