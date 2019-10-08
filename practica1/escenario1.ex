56be30193ba8994b39feb0dee01c4f92f33276ec
jtambo99 <jtambo@live.com>
2019-10-08 10:56:13 +0200

    Añadido envio PID cliente

--- practica1/escenario1.ex
+++ practica1/escenario1.ex
@@ -9,19 +9,20 @@ import Fib
 
 defmodule Servidor do
   def server() do
-    {pid, [op | tail]} =
+    {pid, [client_pid,op | tail]} =
       receive do
         l -> l
       end
 
-    fib_list =
+    #fib_list =
       cond do
         op == :fib -> Enum.map(tail, fn x -> Fib.fibonacci(x) end)
         op == :fib_tr -> Enum.map(tail, fn x -> Fib.fibonacci_tr(x) end)
         op == :of -> Enum.map(tail, fn x -> Fib.of(x) end)
       end
 
-    send(pid, fib_list)
-    server()
+    IO.puts tail  
+    #send(client_pid, fib_list)
+    #server()
   end
 end
