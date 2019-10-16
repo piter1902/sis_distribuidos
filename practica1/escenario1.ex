2f91743877c6cf62ec489d5a04287265dd47f289
Pedro Tamargo <pedro.tamargo.allue@gmail.com>
2019-10-16 16:09:09 +0200

    Escenario 1 y 2 cambiados

--- practica1/escenario1.ex
+++ practica1/escenario1.ex
@@ -9,79 +9,86 @@ import Fib
 
 defmodule Servidor do
   def server() do
-    lista_disponibles = [:"w1@10.1.56.75", :"w1@10.1.56.75",:"w1@10.1.56.75", :"w1@10.1.56.75"]
+    lista_disponibles = [:"w1@10.1.56.75", :"w1@10.1.56.75", :"w1@10.1.56.75", :"w1@10.1.56.75"]
     lista_ocupados = []
     lista_pendientes = []
     server(lista_disponibles, lista_ocupados, lista_pendientes)
   end
 
   def server(disp, ocu, pend) do
-    #pid_pool = {:pool, :"pool@10.1.56.75"}
+    # pid_pool = {:pool, :"pool@10.1.56.75"}
     # Escuchamos peticiones del cliente
-    {disp,ocu,pend}=
-    receive do
-      {client, op, limits} -> 
-        if disp != [] do
-          [head | tail] = disp
-          disp = tail
-          ocu = ocu ++ [head]
-          spawn(
-            Servidor,
-            :comunicar,
-            [self(),head,client,op,limits]
-          )
-          {disp,ocu,pend}
-        else
-          pend = pend ++ [{client,op,limits}]
-          IO.puts("Estamos en el caso de no disponibles -> pend = ")
-          IO.puts(inspect(pend))
-          {disp,ocu,pend}
-        end
-      {:fin,pid_w} ->
+    {disp, ocu, pend} =
+      receive do
+        {client, op, limits} ->
+          if disp != [] do
+            [head | tail] = disp
+            disp = tail
+            ocu = ocu ++ [head]
+
+            spawn(
+              Servidor,
+              :comunicar,
+              [self(), head, client, op, limits]
+            )
+
+            {disp, ocu, pend}
+          else
+            pend = pend ++ [{client, op, limits}]
+            IO.puts("Estamos en el caso de no disponibles -> pend = ")
+            IO.puts(inspect(pend))
+            {disp, ocu, pend}
+          end
+
+        {:fin, pid_w} ->
           if pend != [] do
             IO.puts("Hay algun pendiente.")
             # Existe alguien esperando -> Le damos servicio
             [pid_pendiente | resto] = pend
             pend = resto
-            {cliente,op,limits} = pid_pendiente
+            {cliente, op, limits} = pid_pendiente
+
             spawn(
               Servidor,
               :comunicar,
-              [self(),pid_w,cliente,op,limits]
+              [self(), pid_w, cliente, op, limits]
             )
-          {disp,ocu,pend}
-        else
-          # Lo devolvemos a la lista de disponibles
-          IO.puts("No hay ningun pendiente")
-          ocu = ocu -- [pid_w]
-          disp = disp ++ [pid_w]
-          {disp,ocu,pend}
-        end
-    end
-    server(disp,ocu,pend)
+
+            {disp, ocu, pend}
+          else
+            # Lo devolvemos a la lista de disponibles
+            IO.puts("No hay ningun pendiente")
+            ocu = ocu -- [pid_w]
+            disp = disp ++ [pid_w]
+            {disp, ocu, pend}
+          end
+      end
+
+    server(disp, ocu, pend)
   end
 
-  def comunicar(pid_server,pid_w,pid_client,op,limits) do
-    #Generamos el proceso en el nodo y guardamos resultado en la variable resutl
-    
+  def comunicar(pid_server, pid_w, pid_client, op, limits) do
+    # Generamos el proceso en el nodo y guardamos resultado en la variable resutl
+
     Node.spawn(
       pid_w,
       Worker,
       :worker,
-      [self(),pid_w,pid_server,op,Enum.to_list(limits)]
+      [self(), pid_w, pid_server, op, Enum.to_list(limits)]
     )
-    
-    result=
-    receive do
-      result -> result
-    end
+
+    result =
+      receive do
+        result -> result
+      end
 
     IO.puts(inspect(result))
-    
+
     send(
       pid_client,
-      {:fin,result}
+      {:fin, result}
     )
+
     IO.puts("Muerte de comunicar")
   end
 end
@@ -90,24 +97,26 @@ defmodule Worker do
   def worker(pid_thread, pid_w, pid_master, op, lista) do
     # Miramos peticion
     IO.puts("Soy el worker #{pid_w}")
-    result=
-    cond do
-      op == :fib -> Enum.map(lista, fn x -> Fib.fibonacci(x) end)
-      op == :fib_tr -> Enum.map(lista, fn x -> Fib.fibonacci_tr(x) end)
-      op == :of -> Enum.map(lista, fn x -> Fib.of(x) end)
-    end
+
+    result =
+      cond do
+        op == :fib -> Enum.map(lista, fn x -> Fib.fibonacci(x) end)
+        op == :fib_tr -> Enum.map(lista, fn x -> Fib.fibonacci_tr(x) end)
+        op == :of -> Enum.map(lista, fn x -> Fib.of(x) end)
+      end
+
     # Nos ponemos disponibles
     IO.puts("Envio a master que estoy libre")
+
     send(
       pid_master,
-      {:fin,pid_w}
+      {:fin, pid_w}
     )
 
-   # Devolvemos resultado -> Enviando a thread
-   send(
-     pid_thread,
-     result
-   ) 
-
+    # Devolvemos resultado -> Enviando a thread
+    send(
+      pid_thread,
+      result
+    )
   end
 end
