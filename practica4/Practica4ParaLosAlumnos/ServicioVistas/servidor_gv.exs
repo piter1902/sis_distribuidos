6e4c411c11ea9b115c51e269fcc6628cb3460b36
Pedro Tamargo <pedro.tamargo.allue@protonmail.com>
2019-11-22 18:10:57 +0100

    Ficheros de la practica 4

---
+++ practica4/Practica4ParaLosAlumnos/ServicioVistas/servidor_gv.exs
@@ -0,0 +1,102 @@
+require IEx # Para utilizar IEx.pry
+
+defmodule ServidorGV do
+    @moduledoc """
+        modulo del servicio de vistas
+    """
+
+    # Tipo estructura de datos que guarda el estado del servidor de vistas
+    # COMPLETAR  con lo campos necesarios para gestionar
+    # el estado del gestor de vistas
+    defstruct   ??????????????
+
+    # Constantes
+    @latidos_fallidos 4
+
+    @intervalo_latidos 50
+
+
+    @doc """
+        Acceso externo para constante de latidos fallios
+    """
+    def latidos_fallidos() do
+        @latidos_fallidos
+    end
+
+    @doc """
+        acceso externo para constante intervalo latido
+    """
+   def intervalo_latidos() do
+       @intervalo_latidos
+   end
+
+   @doc """
+        Generar un estructura de datos vista inicial
+    """
+    def vista_inicial() do
+        %{num_vista: 0, primario: :undefined, copia: :undefined}
+    end
+
+    @doc """
+        Poner en marcha el servidor para gestión de vistas
+        Devolver atomo que referencia al nuevo nodo Elixir
+    """
+    @spec startNodo(String.t, String.t) :: node
+    def startNodo(nombre, maquina) do
+                                         # fichero en curso
+        NodoRemoto.start(nombre, maquina, __ENV__.file)
+    end
+
+    @doc """
+        Poner en marcha servicio trás esperar al pleno funcionamiento del nodo
+    """
+    @spec startService(node) :: boolean
+    def startService(nodoElixir) do
+        NodoRemoto.esperaNodoOperativo(nodoElixir, __MODULE__)
+        
+        # Poner en marcha el código del gestor de vistas
+        Node.spawn(nodoElixir, __MODULE__, :init_sv, [])
+   end
+
+    #------------------- FUNCIONES PRIVADAS ----------------------------------
+
+    # Estas 2 primeras deben ser defs para llamadas tipo (MODULE, funcion,[])
+    def init_sv() do
+        Process.register(self(), :servidor_gv)
+
+        spawn(__MODULE__, :init_monitor, [self()]) # otro proceso concurrente
+
+        #### VUESTRO CODIGO DE INICIALIZACION
+
+        bucle_recepcion(??????????)
+    end
+
+    def init_monitor(pid_principal) do
+        send(pid_principal, :procesa_situacion_servidores)
+        Process.sleep(@intervalo_latidos)
+        init_monitor(pid_principal)
+    end
+
+
+    defp bucle_recepcion(???????????) do
+        ?????? = receive do
+                    {:latido, n_vista_latido, nodo_emisor} ->
+                
+                        ### VUESTRO CODIGO
+                
+                    {:obten_vista_valida, pid} ->
+
+                        ### VUESTRO CODIGO                
+
+                    :procesa_situacion_servidores ->
+                
+                        ### VUESTRO CODIGO
+
+        end
+
+        bucle_recepcion(??????????)
+    end
+    
+    # OTRAS FUNCIONES PRIVADAS VUESTRAS
+
+end
