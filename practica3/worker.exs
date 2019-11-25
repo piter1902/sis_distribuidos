3da8b945b81d4699e4601717203c7b6b821a58c1
jtambo99 <jtambo@live.com>
2019-11-25 18:36:37 +0100

    Resultados de ambos clientes mas sus versiones

--- practica3/worker.exs
+++ practica3/worker.exs
@@ -77,97 +77,131 @@ defmodule Worker do
   end
 end
 
+# defmodule Cliente do
+#   def server_mapa(mapa) do
+#     mapa =
+#       receive do
+#         {:set, where, value} ->
+#           Map.put(mapa, where, value)
+
+#         {:get, pid} ->
+#           send(
+#             pid,
+#             {:get_reply, mapa}
+#           )
+
+#           mapa
+#       end
+
+#     server_mapa(mapa)
+#   end
+
+#   def escucha(server_mapa) do
+#     receive do
+#       {:result, l, nPaquete} ->
+#         send(
+#           server_mapa,
+#           {:get, self}
+#         )
+
+#         mapa =
+#           receive do
+#             {:get_reply, mapa} -> mapa
+#           end
+
+#         t1 = Map.get(mapa, nPaquete)
+#         tFinal = Time.diff(Time.utc_now(), t1, :microsecond)
+#         # IO.puts("Tiempo ahora #{inspect(Time.utc_now())} y tiempo inicial #{inspect(t1)}")
+
+#         IO.puts(
+#           "Envio #{nPaquete} ha tardado #{inspect(tFinal)} microseconds y su valor es #{
+#             inspect(l)
+#           }"
+#         )
+
+#         # IO.inspect(l)
+#     end
+
+#     escucha(server_mapa)
+#   end
+
+#   defp launch(pid, 1, nenvio, pid_server, pid_escucha) do
+#     send(
+#       pid_server,
+#       {:set, nenvio, Time.utc_now()}
+#     )
+
+#     send(pid, {pid_escucha, 1500, nenvio})
+#   end
+
+#   defp launch(pid, n, nenvio, pid_server, pid_escucha) when n != 1 do
+#     number = if rem(n, 3) == 0, do: 100, else: 36
+#     # IO.puts("Introduzco en #{nenvio} el tiempo #{inspect(Time.utc_now())}")
+
+#     send(
+#       pid_server,
+#       {:set, nenvio, Time.utc_now()}
+#     )
+
+#     send(pid, {pid_escucha, :random.uniform(number), nenvio})
+#     launch(pid, n - 1, nenvio + 1, pid_server, pid_escucha)
+#   end
+
+#   defp genera_workload(server_pid, nEnvio, pid_server, pid_escucha) do
+#     launch(server_pid, 6 + :random.uniform(2), nEnvio, pid_server, pid_escucha)
+#     Process.sleep(2000 + :random.uniform(200))
+#     genera_workload(server_pid, nEnvio + 8, pid_server, pid_escucha)
+#   end
+
+#   def init(server_pid) do
+#     pid_server =
+#       spawn(
+#         Cliente,
+#         :server_mapa,
+#         [Map.new()]
+#       )
+
+#     pid_escucha =
+#       spawn(
+#         Cliente,
+#         :escucha,
+#         [pid_server]
+#       )
+
+#     genera_workload(server_pid, 0, pid_server, pid_escucha)
+#   end
+# end
+
 defmodule Cliente do
-  def server_mapa(mapa) do
-    mapa =
-      receive do
-        {:set, where, value} ->
-          Map.put(mapa, where, value)
+  defp launch(pid, 1, mapa, nEnvio) do
+    mapa = Map.put(mapa, nEnvio, Time.utc_now())
+    send(pid, {self, 1500, nEnvio})
 
-        {:get, pid} ->
-          send(
-            pid,
-            {:get_reply, mapa}
-          )
-
-          mapa
-      end
-
-    server_mapa(mapa)
-  end
-
-  def escucha(server_mapa) do
     receive do
       {:result, l, nPaquete} ->
-        send(
-          server_mapa,
-          {:get, self}
-        )
-
-        mapa =
-          receive do
-            {:get_reply, mapa} -> mapa
-          end
-
         t1 = Map.get(mapa, nPaquete)
         tFinal = Time.diff(Time.utc_now(), t1, :microsecond)
-        # IO.puts("Tiempo ahora #{inspect(Time.utc_now())} y tiempo inicial #{inspect(t1)}")
 
         IO.puts(
           "Envio #{nPaquete} ha tardado #{inspect(tFinal)} microseconds y su valor es #{
             inspect(l)
           }"
         )
-
-        # IO.inspect(l)
     end
 
-    escucha(server_mapa)
+    mapa
   end
 
-  defp launch(pid, 1, nenvio, pid_server, pid_escucha) do
-    send(
-      pid_server,
-      {:set, nenvio, Time.utc_now()}
-    )
-
-    send(pid, {pid_escucha, 1500, nenvio})
-  end
-
-  defp launch(pid, n, nenvio, pid_server, pid_escucha) when n != 1 do
+  defp launch(pid, n, mapa, nEnvio) when n != 1 do
     number = if rem(n, 3) == 0, do: 100, else: 36
-    # IO.puts("Introduzco en #{nenvio} el tiempo #{inspect(Time.utc_now())}")
-
-    send(
-      pid_server,
-      {:set, nenvio, Time.utc_now()}
-    )
-
-    send(pid, {pid_escucha, :random.uniform(number), nenvio})
-    launch(pid, n - 1, nenvio + 1, pid_server, pid_escucha)
+    mapa = Map.put(mapa, nEnvio, Time.utc_now())
+    send(pid, {self, :random.uniform(number), nEnvio})
+    launch(pid, n - 1, mapa, nEnvio + 1)
   end
 
-  defp genera_workload(server_pid, nEnvio, pid_server, pid_escucha) do
-    launch(server_pid, 6 + :random.uniform(2), nEnvio, pid_server, pid_escucha)
+  def genera_workload(server_pid, mapa, nEnvio) do
+    mapa = launch(server_pid, 6 + :random.uniform(2), mapa, nEnvio)
     Process.sleep(2000 + :random.uniform(200))
-    genera_workload(server_pid, nEnvio + 8, pid_server, pid_escucha)
-  end
-
-  def init(server_pid) do
-    pid_server =
-      spawn(
-        Cliente,
-        :server_mapa,
-        [Map.new()]
-      )
-
-    pid_escucha =
-      spawn(
-        Cliente,
-        :escucha,
-        [pid_server]
-      )
-
-    genera_workload(server_pid, 0, pid_server, pid_escucha)
+    genera_workload(server_pid, mapa, nEnvio + 8)
   end
 end
